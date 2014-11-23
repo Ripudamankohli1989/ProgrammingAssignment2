3163161ce27a412286de2df0ea0b3a98fd89d4f4
Ripudaman Singh Kohli <Ripudamankohli1989@gmail.com>
2014-11-23 13:55:10 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x=matrix()) {
+    m <- NULL
+    get <- function() x
+    setImatrix <- function(Imatrix) m <<- Imatrix
+    getImatrix <- function() m
 
+    # return a list of functions as an R object
+    list(get=get, setImatrix=setImatrix, getImatrix=getImatrix)
 }
+
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    m <- x$getImatrix()
+    if(!is.null(m)){
+        message("Cached data found. Getting result... Done.")
+        return(m)
+    }
+    else {
+        message("No cached data found. Calculating inverse matrix...")
+        data <- x$get() # obtains matrix from object x
+        m <- solve(data) # finds inverse matrix
+        x$setImatrix(m) # assigns resulting inverse matrix to object x
+        message("Done.")
+        return(m)
+    }
 }
